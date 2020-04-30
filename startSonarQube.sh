9da381fbc3b8e62f02bf3a37a6feef19899a96d2
Scott Paige <scottpaige1@gmail.com>
2020-04-30 09:34:47 -0700

    Working on grabbing vcap_services

--- startSonarQube.sh
+++ startSonarQube.sh
@@ -4,15 +4,16 @@ echo "-----> Making java available"
 export PATH=$PATH:/home/vcap/app/.java/bin
 
 echo "-----> Setting sonar.properties"
-export SONARQUBE_JDBC_USERNAME=`node getCreds.js username`
-export SONARQUBE_JDBC_PASSWORD=`node getCreds.js password`
-export SONARQUBE_JDBC_URL=`node getCreds.js jdbcUrl`
-# mySQL doesn't like the url unless it has the appended parameters
-export SONARQUBE_JDBC_URL="$SONARQUBE_JDBC_URL&useUnicode=true&characterEncoding=utf8"
+# export SONARQUBE_JDBC_USERNAME=`node getCreds.js username`
+# export SONARQUBE_JDBC_PASSWORD=`node getCreds.js password`
+# export SONARQUBE_JDBC_URL=`node getCreds.js jdbcUrl`
+# # mySQL doesn't like the url unless it has the appended parameters
+# export SONARQUBE_JDBC_URL="$SONARQUBE_JDBC_URL&useUnicode=true&characterEncoding=utf8"
 
 echo "HERE ARE THE VCAP VARS SONARQUBE_JDBC_USERNAME=$SONARQUBE_JDBC_USERNAME SONARQUBE_JDBC_PASSWORD=$SONARQUBE_JDBC_PASSWORD SONARQUBE_JDBC_URL=$SONARQUBE_JDBC_URL"
 
-export username="$VCAP_SERVICES['p.mysql'][0]['credentials']['username']"
+export username="$VCAP_SERVICES[${p.mysql}][0][$credentials][$username]"
+
 echo "USERNAME: $username"
 echo "       sonar.web.port=${SONARQUBE_PORT}"
 echo "\n ------- The following properties were automatically created by the buildpack -----\n" >> ./sonar.properties
