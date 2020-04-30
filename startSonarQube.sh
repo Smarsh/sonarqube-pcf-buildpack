49b7cf97640aeca327cacf763644c6d9803032bf
Scott Paige <scottpaige1@gmail.com>
2020-04-30 08:41:53 -0700

    Debugging

--- startSonarQube.sh
+++ startSonarQube.sh
@@ -10,6 +10,8 @@ export SONARQUBE_JDBC_URL=`node getCreds.js jdbcUrl`
 # mySQL doesn't like the url unless it has the appended parameters
 export SONARQUBE_JDBC_URL="$SONARQUBE_JDBC_URL&useUnicode=true&characterEncoding=utf8"
 
+echo "HERE ARE THE VCAP VARS SONARQUBE_JDBC_USERNAME=$SONARQUBE_JDBC_USERNAME SONARQUBE_JDBC_PASSWORD=$SONARQUBE_JDBC_PASSWORD SONARQUBE_JDBC_URL=$SONARQUBE_JDBC_URL"
+
 echo "       sonar.web.port=${SONARQUBE_PORT}"
 echo "\n ------- The following properties were automatically created by the buildpack -----\n" >> ./sonar.properties
 echo "sonar.web.port=${SONARQUBE_PORT}\n" >> ./sonar.properties
