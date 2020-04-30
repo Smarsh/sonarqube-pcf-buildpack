93d14bdee1876beb68cc6125160f4efab06f93e2
Scott Paige <scottpaige1@gmail.com>
2020-04-30 10:46:17 -0700

    Working on grabbing vcap_services

--- startSonarQube.sh
+++ startSonarQube.sh
@@ -10,6 +10,8 @@ DEFAULT_JDBC_URL=`echo $VCAP_SERVICES | jq '.["p.mysql"][]["credentials"]["jdbcU
 # mySQL doesn't like the url unless it has the appended parameters
 export SONARQUBE_JDBC_URL="$DEFAULT_JDBC_URL&useUnicode=true&characterEncoding=utf8"
 
+echo "$SONARQUBE_JDBC_USERNAME $SONARQUBE_JDBC_PASSWORD $SONARQUBE_JDBC_URL"
+
 echo "       sonar.web.port=${SONARQUBE_PORT}"
 echo "\n ------- The following properties were automatically created by the buildpack -----\n" >> ./sonar.properties
 echo "sonar.web.port=${SONARQUBE_PORT}\n" >> ./sonar.properties
