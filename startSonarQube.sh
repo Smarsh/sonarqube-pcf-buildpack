746f6d76b2755edce0052060ccfdbb436b3997c2
Scott Paige <42152812+scott-paige@users.noreply.github.com>
2020-04-30 14:55:52 -0700

    [DEL-1458] added logic that grabs the credentials from vcap (#1)
    * [DEL-1458] added js script that grabs the credentials from vcap
    
    * Update startSonarQube.sh
    
    * Debugging
    
    * Debugging env variables
    
    * Debugging env variables
    
    * Fixed type
    
    * Working on grabbing vcap_services
    
    * Working on grabbing vcap_services
    
    * Working on grabbing vcap_services
    
    * Working on grabbing vcap_services
    
    * Working on grabbing vcap_services
    
    * Working on grabbing vcap_services
    
    * Working on grabbing vcap_services
    
    * Working on grabbing vcap_services
    
    * Working on grabbing vcap_services
    
    * Working on grabbing vcap_services
    
    * Working on grabbing vcap_services
    
    * Working on grabbing vcap_services
    
    * Working on grabbing vcap_services
    
    * Working on grabbing vcap_services
    
    * Cleaned up branch
    
    * Update startSonarQube.sh

--- startSonarQube.sh
+++ startSonarQube.sh
@@ -4,6 +4,29 @@ echo "-----> Making java available"
 export PATH=$PATH:/home/vcap/app/.java/bin
 
 echo "-----> Setting sonar.properties"
+vcap_username=`echo $VCAP_SERVICES | jq '.["p.mysql"][]["credentials"]["username"]'`
+vcap_password=`echo $VCAP_SERVICES | jq '.["p.mysql"][]["credentials"]["password"]'`
+vcap_jdbc_url=`echo $VCAP_SERVICES | jq '.["p.mysql"][]["credentials"]["jdbcUrl"]'`
+
+#------------------------------------------
+# Drop the " at the beginning and end of the variable
+#------------------------------------------
+
+vcap_jdbc_url="${vcap_jdbc_url%?}"
+vcap_jdbc_url="${vcap_jdbc_url#?}"
+# mySQL doesn't like the url unless it has the appended parameters
+vcap_jdbc_url="${vcap_jdbc_url}&useUnicode=true&characterEncoding=utf8"
+export SONARQUBE_JDBC_URL=$vcap_jdbc_url
+
+vcap_username="${vcap_username%?}"
+vcap_username="${vcap_username#?}"
+export SONARQUBE_JDBC_USERNAME=$vcap_username
+
+vcap_password="${vcap_password%?}"
+vcap_password="${vcap_password#?}"
+export SONARQUBE_JDBC_PASSWORD=$vcap_password
+
+
 echo "       sonar.web.port=${SONARQUBE_PORT}"
 echo "\n ------- The following properties were automatically created by the buildpack -----\n" >> ./sonar.properties
 echo "sonar.web.port=${SONARQUBE_PORT}\n" >> ./sonar.properties
